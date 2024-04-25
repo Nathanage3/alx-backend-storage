bf196b9b90cf8cad62b0a856873c1609574b4e74
Nathanage3 <sisay954@gmail.com>
2024-04-26 02:37:09 +0300

    create table users with ENUM

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -8,8 +8,8 @@
 -- never null (= default will be the first element of the enumeration, here US)
 
 CREATE TABLE IF NOT EXISTS users (
-  id INT AUTOINCREMENT PRIMARY KEY,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  name VARCHAR(255),
-  country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
-)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
